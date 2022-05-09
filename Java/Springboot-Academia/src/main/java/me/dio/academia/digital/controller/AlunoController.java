e673b20f7e348742032398de1bfec7ddeb3059a4
wesleyvelloso <wmarques092@gmail.com>
2022-05-09 19:21:07 -0300

    first commit

---
+++ Java/Springboot-Academia/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -0,0 +1,35 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl alunoService;
+
+    @GetMapping
+    public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false) String dataDeNascimento){
+
+        return alunoService.getAll(dataDeNascimento);
+    }
+
+    @PostMapping
+    public Aluno create(@Valid @RequestBody AlunoForm form){
+        return alunoService.create(form);
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvalicaoFisicaId(@PathVariable Long id){
+        return alunoService.getAllAvaliacaoFisicaId(id);
+    }
+}
